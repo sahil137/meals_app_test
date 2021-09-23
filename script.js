bc91ad53c44fa2ed57734c2d4c7621a36e7a074f
Sahil Saini <46193454+sahil137@users.noreply.github.com>
2021-09-24 01:09:49 +0530

    add get recipe functionality

--- script.js
+++ script.js
@@ -2,10 +2,14 @@
 let searchTextElement = document.getElementById("search-meal-input");
 const searchButton = document.getElementById("search-button");
 const mealSearchResultsContainer = document.getElementById("meal");
+const mealDetailsContainer = document.getElementById("meal-details-container");
+const backButton = document.getElementsByClassName("back-button")[0];
 
 // Event Listeners
 searchButton.addEventListener("click", searchForMeal);
 searchTextElement.addEventListener("input", searchForMeal);
+mealSearchResultsContainer.addEventListener("click", getRecipeDetails);
+backButton.addEventListener("click", closeRecipeDetails);
 
 // Functions
 function searchForMeal() {
@@ -21,7 +25,7 @@ function searchForMeal() {
         // loop over every meal and add it to the list
         data.meals.forEach((meal) => {
           list += `
-              <div class = "meal-item d-flex flex-column align-items-center" meal-id = "${meal.idMeal}">
+              <div class = "meal-item d-flex flex-column align-items-center" id = "${meal.idMeal}">
                   <div class = "meal-img">
                       <img src = "${meal.strMealThumb}" alt = "food">
                   </div>
@@ -37,4 +41,44 @@ function searchForMeal() {
       // append all meals found to meal div
       mealSearchResultsContainer.innerHTML = list;
     });
+}
+
+// To display recipe details
+function getRecipeDetails(event) {
+  // event.preventDefault();
+
+  if (event.target.classList.contains("recipe-button")) {
+    let mealItem = event.target.parentElement.parentElement;
+    console.log(mealItem);
+    const mealId = mealItem.id;
+    let content = ``;
+    fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`)
+      .then((response) => response.json())
+      .then((data) => addRecipeDetail(data.meals[0]));
+
+    // console.log(mealDetailsContainer.parentElement);
+  }
+}
+
+function addRecipeDetail(meal) {
+  let content = `
+    <h2 class = "recipe-title">${meal.strMeal}</h2>
+    <div class = "recipe-instructions">
+        <h3>Instructions:</h3>
+        <p>${meal.strInstructions}</p>
+    </div>
+    <div class = "recipe-meal-img">
+        <img src = "${meal.strMealThumb}" alt = "">
+    </div>
+    <div class = "recipe-link">
+        <a href = "${meal.strYoutube}" target = "_blank">Watch Video</a>
+    </div>`;
+  mealDetailsContainer.innerHTML = content;
+  mealDetailsContainer.parentElement.classList.add("show-recipe");
+}
+
+// To close recipe details
+function closeRecipeDetails() {
+  mealDetailsContainer.parentElement.classList.remove("show-recipe");
+  // console.log(mealDetailsContainer.parentElement);
 }
